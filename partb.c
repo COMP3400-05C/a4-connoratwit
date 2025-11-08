e6a33ecd44a9c6f4ac9696aada10a332b0413893
connoratwit <okeefec@wit.edu>
2025-11-07 21:38:49 -0500

    Completed A4 implementation

--- partb.c
+++ partb.c
@@ -1,8 +1,49 @@
 #include <stdbool.h>
 #include <stddef.h>
-
-// TODO: Include more header files (read the manpages to find out more!)
+#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, const char* argv[]) {
+    // Check if there are any arguments
+    if (argc < 2) {
+        printf("ERROR: no arguments\n");
+        return 1;
+    }
+    
+    // Calculate number of actual arguments
+    int num_args = argc - 1;
+    
+    // Determine middle argument(s)
+    int start_idx, end_idx;
+    
+    if (num_args % 2 == 1) {
+        // Odd number of arguments: take the middle one
+        start_idx = (num_args / 2) + 1;
+        end_idx = start_idx;
+    } else {
+        // Even number of arguments: take the middle two
+        start_idx = (num_args / 2);
+        end_idx = start_idx + 1;
+    }
+    
+    // Build the arguments array for echo
+    int num_echo_args = (end_idx - start_idx + 1);
+    char* eargs[num_echo_args + 2];  // +1 for "echo", +1 for NULL
+    
+    eargs[0] = "echo";
+    for (int i = start_idx, j = 1; i <= end_idx; i++, j++) {
+        eargs[j] = (char*)argv[i];
+    }
+    eargs[num_echo_args + 1] = NULL;
+    
+    // Execute echo command
+    int eret = execv("/bin/echo", eargs);
+    
+    // If exec returns, there was an error
+    if (eret == -1) {
+        perror("exec");
+        return 1;
+    }
+    
     return 0;
 }
--- partc.c
+++ partc.c
@@ -1,9 +1,74 @@
 #include <stdbool.h>
 #include <stddef.h>
-
-// TODO: Include more header files (read the manpages to find out more!)
+#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, const char* argv[]) {
-    // TODO: Complete and document
+    // Check if we have exactly 3 arguments: program_name, word, filename
+    if (argc < 3) {
+        printf("ERROR: no arguments\n");
+        return 1;
+    }
+    
+    const char* search_word = argv[1];
+    const char* filename = argv[2];
+    
+    // Fork a child process
+    pid_t pid = fork();
+    
+    if (pid == -1) {
+        // Fork failed
+        perror("fork");
+        return 1;
+    }
+    
+    if (pid == 0) {
+        // Child process: run grep
+        char* grep_args[] = {
+            "grep",
+            "-s",           // Suppress error messages
+            "-q",           // Quiet mode (no output)
+            (char*)search_word,
+            (char*)filename,
+            NULL
+        };
+        
+        execv("/usr/bin/grep", grep_args);
+        
+        // If exec returns, there was an error
+        perror("exec");
+        return 1;
+    } else {
+        // Parent process: wait for child
+        int status;
+        waitpid(pid, &status, 0);
+        
+        // Check if child exited normally
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+            
+            if (exit_status == 0) {
+                // Word found
+                printf("FOUND: %s\n", search_word);
+                return 0;
+            } else if (exit_status == 1) {
+                // Word not found (no match)
+                printf("NOT FOUND: %s\n", search_word);
+                return 0;
+            } else if (exit_status == 2) {
+                // File doesn't exist or can't be read
+                printf("ERROR: %s doesn't exist\n", filename);
+                return 2;
+            } else {
+                // Other error
+                printf("ERROR: grep exited with status %d\n", exit_status);
+                return 2;
+            }
+        } else {
+            printf("ERROR: child process did not exit normally\n");
+            return 1;
+        }
+    }
+    
     return 0;
 }
