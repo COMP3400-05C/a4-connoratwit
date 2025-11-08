960090e301c4686feb3537eac49be35ee157b24e
finitesphere <Tobin_H123@protonmail.com>
2025-11-07 21:37:55 -0500

    Completed Part A

--- parta.c
+++ parta.c
@@ -1,8 +1,30 @@
 #include <stdbool.h>
 #include <stddef.h>
-
-// TODO: Include more header files (read the manpages to find out more!)
+#include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, const char* argv[]) {
+    // Check if there are any arguments
+    if (argc < 2) {
+        printf("ERROR: No arguments\n");
+        return 1;
+    }
+    
+    // Loop through all arguments starting from index 1 
+    for (int i = 1; i < argc; i++) {
+        // Print each character of the argument in uppercase
+        for (int j = 0; argv[i][j] != '\0'; j++) {
+            putchar(toupper(argv[i][j]));
+        }
+        
+        // Print comma separator between arguments 
+        if (i < argc - 1) {
+            putchar(',');
+        }
+    }
+    
+    // Print newline at the end
+    putchar('\n');
+    
     return 0;
 }
